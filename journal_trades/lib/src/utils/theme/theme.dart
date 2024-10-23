baa79fd3a5ca901dd65c2cd52e8c14e83111fc49
Gabriel265 <gabrielkadiwa@gmail.com>
2024-10-23 09:36:12 +0200

    add themes - removed unused import and commented example lines

--- journal_trades/lib/src/utils/theme/theme.dart
+++ journal_trades/lib/src/utils/theme/theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:journal_trades/src/utils/theme/widget_themes/text_theme.dart';
 
 class TAppTheme {
@@ -8,19 +7,6 @@ class TAppTheme {
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     textTheme: TTextTheme.lightTextTheme,
-
-    // primarySwatch: const MaterialColor(0xffffe200, <int, Color>{
-    //   50: Color(0x1AFFE200),
-    //   100: Color(0x33ffe200),
-    //   200: Color(0x4dffe200),
-    //   300: Color(0x66ffe200),
-    //   400: Color(0x80ffe200),
-    //   500: Color(0xffffe200),
-    //   600: Color(0x99ffe200),
-    //   700: Color(0xb3ffe200),
-    //   800: Color(0xccffe200),
-    //   900: Color(0xe6ffe200),
-    // }),
   );
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
